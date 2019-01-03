76bd258a7f6362dc7d51837ed7528a9af8eea59c
BIT <BIT@DESKTOP-E23PFRN>
2019-01-03 19:25:48 +0900

    190103 19:25 Prob5 Complete 다시 확인 해볼것!

--- src/prob5/MyStack.java
+++ src/prob5/MyStack.java
@@ -1,17 +1,37 @@
 package prob5;
 
+import java.util.Arrays;
+
 public class MyStack {
 	private int top;
 	private String[] buffer;
 
 	@SuppressWarnings("unchecked")
 	public MyStack(int capacity) {
+		top = -1;
+		buffer = new String[capacity];
 	}
 
 	public void push(String s) {
+		top++;
+		if(top == buffer.length) {
+			buffer = Arrays.copyOf(buffer, buffer.length+1);
+			buffer[top] = s;
+			
+		} else {
+			buffer[top] = s;
+		}
 	}
 
 	public String pop() throws MyStackException {
+		
+		if(top == -1) {
+			throw new MyStackException("stack is empty");
+		}
+		String result = buffer[top];
+		top--;
+		return result;
+		
 	}
 
 	public boolean isEmpty() {
---
+++ src/prob5/MyStackException.java
@@ -0,0 +1,10 @@
+package prob5;
+
+public class MyStackException extends Exception{
+	private static final long serialVersionUID = -4345544702249838346L;
+	
+	public MyStackException(String message) {
+		super(message);
+	}
+
+}
